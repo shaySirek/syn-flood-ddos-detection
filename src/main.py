4c6d078f18f1e194cd2d668d73422e6f869f7486
shaysirek <shaysirek@gmail.com>
2023-03-11 23:17:38 +0200

    - :necktie: use sources_per_dest instead of freqs - :loud_sound: update logs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from dcs import DistinctCountSketch, int2ip, ip2int
+from dcs import DistinctCountSketch, ip2int
 from ddos_dataset import (
     BalancedDataset,
     SynthDataset,
@@ -12,41 +12,41 @@ def main():
     dataset = SynthDataset()
     sources_per_dest = dataset.naive_syn_flood_detection()
     sources_per_dest.to_csv("synth_ddos_results.csv")
-    
-    """
-    dataset = BOUN_DDoS_Dataset()
-    sources_per_dest = dataset.naive_syn_flood_detection()
-    sources_per_dest.to_csv("boun_ddos_results.csv")
-    """
-    
+
     """
     dataset = BalancedDataset()
     sources_per_dest = dataset.naive_syn_flood_detection()
     sources_per_dest.to_csv("balanced_ddos_results.csv")
     """
-   
+
     # user defined parameters
     delta = 0.05
     epsilon = 1 / 3
     k = 3
-    print(f"delta={delta}, epsilon={epsilon}, k={k}")
 
-    params, freqs = dataset.calc_params(delta, epsilon, k)
-    
+    # calculate sketch params - driven by the data
+    sketch_params = dataset.calc_params(delta, epsilon, k)
+
     # init the sketch
     sketch = DistinctCountSketch()
-    # sketch = DistinctCountSketch(**params)
+    # sketch = DistinctCountSketch(**sketch_params)
 
     # sketch the stream
     sketch.record_stream(dataset.df, dataset.get_row)
+    
+    # print First-level buckets
+    print(100*'*')
+    print("First-level buckets:")
     for b, s in enumerate(sketch.h_recorder):
         print(f"{b} -> {len(s)} pairs")
+    
+    print(100*'*')
+    print("Estimations:")
 
-    for t in [None, 50, 100, 200]:
-        print(f"threshold={t}")
-        
+    # try with different thresholds
+    for threshold in [None, 50, 100, 200]:
         # query the sketch
-        top_k_ddos_victims = sketch.top_k(epsilon, k, threshold=t)
+        top_k_ddos_victims = sketch.top_k(epsilon, k, threshold=threshold)
 
         # analyze results
         for victim in top_k_ddos_victims:
@@ -54,12 +54,10 @@ def main():
             int_ip = ip2int(ip)
 
             # get real frequency
-            idx = np.argwhere(freqs.index == int_ip)
-            real_f = freqs.values[idx]
+            idx = np.argwhere(sources_per_dest.index == int_ip)
+            real_f = sources_per_dest.values[idx]
 
             print(f"ip={ip}, idx={idx}, f={real_f}, estimated={est_f}")
-            
-        print(100*'*')
 
 
 if __name__ == '__main__':
